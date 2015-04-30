e2d95899b719f37c53072111461a65d038aaa9c7
Daniel Chen <Maveri2k@yahoo.com>
2015-04-29 21:42:01 -0400

    init sc and standalone python code

---
+++ src/ch-02/init_sc.py
@@ -0,0 +1,19 @@
+from pyspark import SparkConf, SparkContext
+
+# for standalone apps, you need to create a sparkcontext (sc) first
+conf = SparkConf().setMaster("local").setAppName("my script")
+sc = SparkContext(appName="my script")
+
+# create an RDD called lines
+lines = sc.textFile("README.md")
+print("This is the value of 'lines.count()' from the python script: {} ~~~~~~~"
+      .format(lines.count()))
+
+print("This is the value of 'lines.first()' from the python script: {} ~~~~~~~"
+      .format(lines.first()))
+
+# filtering
+lines = sc.textFile("README.md")
+pythonLines = lines.filter(lambda line: "Python" in line)
+print("This is the value of 'pythonLines.first()' form the python script: {}"
+      .format(pythonLines.first()))
